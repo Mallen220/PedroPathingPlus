b5753c950d810ea6599939cf84de06ca84a36890
Mallen220 <allenmc220@gmail.com>
2025-12-25 18:18:32 -0600

    Add Maven publishing and JitPack support
    Added maven-publish plugin and publication configuration to app/build.gradle.kts to generate AAR and POM for JitPack. Introduced jitpack.yml to specify Java 17 and automate publishing tasks. Added CHANGELOG.md to document project changes.

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+## v1.0.4 - 2025-12-25
+
+Force .pom detection. 
+
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.androidLibrary)
+    id("maven-publish")
 }
 
 android {
@@ -25,6 +26,55 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+// Publish to mavenLocal so JitPack (and local users) can get the generated POM/JAR.
+// Create a publication from the Android 'release' component. JitPack will run
+// `./gradlew ... publishToMavenLocal` and expect a POM named <artifactId>-<version>.pom.
+afterEvaluate {
+    publishing {
+        publications {
+            create<org.gradle.api.publish.maven.MavenPublication>("maven") {
+                artifactId = "PedroPathingPlus"
+                // Use the project group and version as provided (JitPack sets -Pgroup/-Pversion when invoking)
+                // Attach the AAR produced by the Android library module. The AAR is created by the "assembleRelease" task.
+                val aarFile = file("$buildDir/outputs/aar/${project.name}-release.aar")
+                artifact(aarFile) {
+                    builtBy(tasks.named("assembleRelease"))
+                }
+
+                // Set minimal coordinates if not defined
+                if (project.group.toString().isNotEmpty()) {
+                    groupId = project.group.toString()
+                }
+                if (project.version.toString().isNotEmpty()) {
+                    version = project.version.toString()
+                }
+
+                // Provide a simple POM customization (optional)
+                pom {
+                    name.set("${project.name}")
+                    description.set("PedroPathingPlus Android library")
+                    url.set("https://github.com/${project.findProperty("githubOwner") ?: "Mallen220"}/${project.findProperty("githubRepo") ?: "PedroPathingPlus"}")
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("Mallen220")
+                            name.set("Mallen220")
+                        }
+                    }
+                }
+            }
+        }
+        repositories {
+            mavenLocal()
+        }
     }
 }
 
